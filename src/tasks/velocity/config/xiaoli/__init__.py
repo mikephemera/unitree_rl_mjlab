e6f05b3c39847bdcdf18c21bcf2ff4967ce3727a
Michael Liang <michaelly0128@gmail.com>
2026-03-20 19:38:48 +0800

    Xiaoli flat rename and train

--- src/tasks/velocity/config/xiaoli/__init__.py
+++ src/tasks/velocity/config/xiaoli/__init__.py
@@ -2,23 +2,23 @@ from mjlab.tasks.registry import register_mjlab_task
 from mjlab.tasks.velocity.rl import VelocityOnPolicyRunner
 
 from .env_cfgs import (
-  xiaoli_flat_env_cfg,
-  xiaoli_rough_env_cfg,
+    xiaoli_flat_env_cfg,
+    xiaoli_rough_env_cfg,
 )
 from .rl_cfg import xiaoli_ppo_runner_cfg
 
 register_mjlab_task(
-  task_id="Mjlab-Velocity-Rough-Xiaoli",
-  env_cfg=xiaoli_rough_env_cfg(),
-  play_env_cfg=xiaoli_rough_env_cfg(play=True),
-  rl_cfg=xiaoli_ppo_runner_cfg(),
-  runner_cls=VelocityOnPolicyRunner,
+    task_id="Xiaoli-Velocity-Rough",
+    env_cfg=xiaoli_rough_env_cfg(),
+    play_env_cfg=xiaoli_rough_env_cfg(play=True),
+    rl_cfg=xiaoli_ppo_runner_cfg(),
+    runner_cls=VelocityOnPolicyRunner,
 )
 
 register_mjlab_task(
-  task_id="Mjlab-Velocity-Flat-Xiaoli",
-  env_cfg=xiaoli_flat_env_cfg(),
-  play_env_cfg=xiaoli_flat_env_cfg(play=True),
-  rl_cfg=xiaoli_ppo_runner_cfg(),
-  runner_cls=VelocityOnPolicyRunner,
+    task_id="Xiaoli-Velocity-Flat",
+    env_cfg=xiaoli_flat_env_cfg(),
+    play_env_cfg=xiaoli_flat_env_cfg(play=True),
+    rl_cfg=xiaoli_ppo_runner_cfg(),
+    runner_cls=VelocityOnPolicyRunner,
 )
